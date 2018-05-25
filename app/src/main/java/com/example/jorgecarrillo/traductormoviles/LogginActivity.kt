2db54d3c9468ac9c2d3a2e4b4a41c3d94ad3fa1e
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-05-25 08:54:20 -0500

    Creación del Proyecto

---
+++ app/src/main/java/com/example/jorgecarrillo/traductormoviles/LogginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.jorgecarrillo.traductormoviles
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.FirebaseAuth
+import android.support.annotation.NonNull
+
+
+
+
+class LogginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loggin)
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/jorgecarrillo/traductormoviles/MainActivityTraductor.kt
@@ -0,0 +1,12 @@
+package com.example.jorgecarrillo.traductormoviles
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityTraductor : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_traductor)
+    }
+}
